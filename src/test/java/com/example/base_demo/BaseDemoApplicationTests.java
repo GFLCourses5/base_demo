47ca6e0d0479b10cce3c1e33ac211246bbfd1863
akedyk <artem.kedyk@geeksforless.com>
2023-08-17 10:39:12 +0300

    - Init application.

---
+++ src/test/java/com/example/base_demo/BaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.base_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
