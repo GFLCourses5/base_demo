ac5635d049acc697378cb34f5c34faef307b1eac
Oleksii <mail.alexbondarenko@gmail.com>
2023-08-19 18:47:34 +0300

    add test

--- src/test/java/com/example/base_demo/UserDTOTest.java
+++ src/test/java/com/example/base_demo/UserDTOTest.java
@@ -25,4 +25,10 @@ class UserDTOTest {
         Assertions.assertNull(userDTO.getB1());
         Assertions.assertFalse(userDTO.isB2());
     }
+
+    @Test
+    public void testToString() {
+        UserDTO userDTO = new UserDTO(1, "test name" , true, false);
+        Assertions.assertEquals("UserDTO{id=1, name='test name', b1=true, b2=false}", userDTO.toString());
+    }
 }
